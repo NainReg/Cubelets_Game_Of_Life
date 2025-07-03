339d368d7e78c94e45b0a42222d1ad8a0afa01cf
Dan <146668550+NainReg@users.noreply.github.com>
2025-07-03 10:03:43 -0600

    Add files via upload

---
+++ codigosSapo/juegaDeLaVida5Sapo.c
@@ -0,0 +1,37 @@
+#include "cubelet.h"
+#include <stdint.h>
+#include "routing_table.h"
+
+void setup() {
+    set_flashlight(0);
+    block_value = 0;
+    wait(500);
+}
+
+void loop() {
+    uint8_t aux1 = get_block_value(1370425);
+    uint8_t aux2 = get_block_value(820396);
+    uint8_t aux3 = get_block_value(923624);
+    uint8_t aux4 = get_block_value(1458295);
+    uint8_t aux5 = get_block_value(1458486);//
+    //uint8_t aux6 = get_block_value(1458286);
+ //   uint8_t aux7 = get_block_value(1407280);
+//    uint8_t aux8 = get_block_value(1364997);
+    //uint8_t aux9 = get_block_value(1365097);
+    
+    uint8_t sum = aux1 + aux2 + aux3 + aux4 + aux5;
+    if(sum == 3 && block_value == 0){
+        set_flashlight(30);
+        block_value = 1;
+    } else if(sum > 3 && block_value == 1){
+        set_flashlight(0);
+        block_value = 0;
+    } else if(sum <= 1 && block_value == 1){
+        set_flashlight(0);
+        block_value = 0;
+    } else if((sum == 2 || sum == 3) && block_value == 1){
+        set_flashlight(30);
+        block_value = 1;
+    }
+    wait(500);
+}
---
+++ codigosTopePlaneador/juegaDeLaVida18Planeador.c
@@ -0,0 +1,37 @@
+#include "cubelet.h"
+#include <stdint.h>
+#include "routing_table.h"
+
+void setup() {
+    set_flashlight(0);
+    block_value = 0;
+    wait(500);
+}
+
+void loop() {
+    uint8_t aux1 = get_block_value(1457307);
+    uint8_t aux2 = get_block_value(1458297);//*
+    uint8_t aux3 = get_block_value(1408211);
+    uint8_t aux4 = get_block_value(1365046);//*
+//    uint8_t aux5 = get_block_value(929836);
+    uint8_t aux6 = get_block_value(1458561);//*
+    uint8_t aux7 = get_block_value(1370608);//*
+    uint8_t aux8 = get_block_value(1370425);//*
+    uint8_t aux9 = get_block_value(1365097);//*
+    
+    uint8_t sum = aux1 + aux2 + aux3 + aux4 + aux9 + aux6 + aux7 + aux8;
+    if(sum == 3 && block_value == 0){
+        set_flashlight(100);
+        block_value = 1;
+    } else if(sum > 3 && block_value == 1){
+        set_flashlight(0);
+        block_value = 0;
+    } else if(sum <= 1 && block_value == 1){
+        set_flashlight(0);
+        block_value = 0;
+    } else if((sum == 2 || sum == 3) && block_value == 1){
+        set_flashlight(100);
+        block_value = 1;
+    }
+    wait(500);
+}
